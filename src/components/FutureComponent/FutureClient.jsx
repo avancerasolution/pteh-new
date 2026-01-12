81eaaab5598e8a34ee5ee52774f0a0ffc7aa4102
Syed Hassan Ali <hassan.ali@avancerasolutions.com>
2026-01-12 23:15:29 +0500

    desktop final

--- src/components/FutureComponent/FutureClient.jsx
+++ src/components/FutureComponent/FutureClient.jsx
@@ -36,7 +36,6 @@ export default function FutureClient() {
           }
         >
           <Banner />
-          {/* <ExecutiveColumns /> */}
           <FutureColumn />
         </Suspense>
       )}
